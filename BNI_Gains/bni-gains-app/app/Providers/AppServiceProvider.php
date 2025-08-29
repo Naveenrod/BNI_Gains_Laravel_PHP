339131d067ddfc600eac321e82053357de0d84e6
NaveenRod <naveen.rodrigo@icloud.com>
2025-08-30 06:30:06 +1000

    AppServiceProvider

--- BNI_Gains/bni-gains-app/app/Providers/AppServiceProvider.php
+++ BNI_Gains/bni-gains-app/app/Providers/AppServiceProvider.php
@@ -2,31 +2,23 @@
 
 namespace App\Providers;
 
-use App\Models\Profile;
-use App\Policies\ProfilePolicy;
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * The policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
+     * Register any application services.
      */
-    protected $policies = [
-        Profile::class => ProfilePolicy::class,
-    ];
+    public function register(): void
+    {
+        //
+    }
 
     /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
+     * Bootstrap any application services.
      */
-    public function boot()
+    public function boot(): void
     {
-        $this->registerPolicies();
-
         //
     }
 }
